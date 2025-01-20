23cb9af1454d95be06795e2dd58c2e17379a24f6
Oystein T <taskjelle@gmail.com>
2025-01-20 21:34:29 +0100

    First version of the build script

--- build.sh
+++ build.sh
@@ -2,17 +2,16 @@
 # frontend build script
 # add version-parameter to .js-files
 # first, iterate all files
-#     # second, if it is a js- or html-file,
+# second, if it is a js- or html-file,
 echo "Adding versioning string to frontend static files to avoid cache issues"
 for file in ./wwwroot/**/*(.);
     if [[ $file == *.js || $file == *.html ]]; then
         echo -n "$file ";
+        # get checksum of file
         hash=$(shasum -a 256 $file | awk '{print $1}')
-        shorthash=${hash:0:10}
+        # utilizing the 16 msb digits
+        shorthash=${hash:0:16}
         echo " $shorthash"
-        #(\w|\d)+(.js)+(\?version=(\w|\d)*)*
-
+        # adding the short hash as versioning string to all .js files to avoid potential caching were not wanteds
+        sed -Er -i.bu "s/([a-z]|[A-Z]|\d|^\s+)(\.js){1}(\?version=)?([a-z]|[A-Z]|[0-9])*/\1\2\?version=$shorthash/g" $file
     fi
-# calculate hash of file
-# go through all the content and replace
-# (\d|\c|\\/)(+\.js) with {1}{2}?version=hash
